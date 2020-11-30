ce9aa952dbe5123ccd432b2ed8f1183d0541e4b4
Hans <hans.kristjan.veri@gmail.com>
2020-11-30 20:28:28 +0200

    js commentid ja õige järjekord särkidel

--- galerii.js
+++ galerii.js
@@ -1,4 +1,6 @@
+//Alustan esimesest pildist
 var slideIndex = 1;
+//Näita esimest pilti
 showSlides(slideIndex);
 
 // Järgmise/eelmise kontrollid
@@ -14,15 +16,15 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  //Kui n on suurem kui elementide arv, siis algusesse tagasi
   if (n > slides.length) {slideIndex = 1}
+  //Kui n on 1-st väiksem, siis seatakse see elementide arvuks, et minna viimasele tagasi
   if (n < 1) {slideIndex = slides.length}
+  //Peidab ülejäänud pildid
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+  //Kuvab slideindexiga pilti
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+
 }
